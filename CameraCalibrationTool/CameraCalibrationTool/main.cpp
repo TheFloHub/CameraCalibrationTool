0e966cc61b9ba8afd9bc75913046735e0e594312
Flo <florian_feuerstein@web.de>
2017-05-23 12:45:35 +0200

    added a circle test

--- CameraCalibrationTool/CameraCalibrationTool/main.cpp
+++ CameraCalibrationTool/CameraCalibrationTool/main.cpp
@@ -2,6 +2,7 @@
 #include <ComputerVisionLib/ImageProcessing/Chessboard/ChessboardCornerDetector.h>
 #include <ComputerVisionLib/ImageProcessing/Chessboard/ChessboardSegmentation.h>
 #include <ComputerVisionLib/Calibration/IntrinsicCameraCalibration.h>
+#include <ComputerVisionLib/Calibration/Circle.h>
 #include <ComputerVisionLib/CameraModel/CameraModel.h>
 #include <ComputerVisionLib/CameraModel/DistortionModel/BrownModel.h>
 #include <ComputerVisionLib/CameraModel/ProjectionModel/PinholeModel.h>
@@ -14,8 +15,42 @@
 #include <chrono>
 #include <string>
 #include <memory>
+#include <random>
 
 using namespace std;
+
+void testCircle()
+{
+	cv::Mat image(400, 400, CV_8UC1);
+	image.setTo(0);
+
+	std::default_random_engine generator;
+	std::normal_distribution<double> distribution(0.0, 4.0);
+
+	double cx = 200.0;
+	double cy = 200.0;
+	double radius = 100.0;
+	size_t numSamples = 200;
+	double angleSpan = 6.3;
+	double angleStep = angleSpan / numSamples;
+	Eigen::Array2Xd points(2, numSamples);
+	double x, y;
+	for (size_t i = 0; i<numSamples; ++i)
+	{
+		x = cx + cos(i*angleStep)*radius + distribution(generator);
+		y = cy + sin(i*angleStep)*radius + distribution(generator);
+		points(0, i) = x;
+		points(1, i) = y;
+		cv::circle(image, cv::Point((int)std::round(x), (int)std::round(y)), 1, 255, 2);
+	}
+
+	Cvl::Circle circle(points, 0, numSamples / 4, numSamples / 2);
+
+	cv::circle(image, cv::Point((int)std::round(circle.getCenter().x()), (int)std::round(circle.getCenter().y())), (int)std::round(circle.getRadius()), 255, 1);
+
+	cv::imshow("Circle", image);
+	cv::waitKey(0);
+}
 
 
 void someTests()
@@ -43,8 +78,8 @@ int main(int argc, char *argv[])
 	}
 
 	std::vector<cv::Mat> images;
-	std::string imagePath = "D:\\Eigene Dokumente\\Visual Studio 2015\\Projects\\Pics\\";
-	//std::string imagePath = "D:\\Projekte\\Pics\\";
+	//std::string imagePath = "D:\\Eigene Dokumente\\Visual Studio 2015\\Projects\\Pics\\";
+	std::string imagePath = "D:\\Projekte\\Pics\\";
 	images.push_back(cv::imread(imagePath + "c1.jpg", cv::IMREAD_GRAYSCALE));
 	images.push_back(cv::imread(imagePath + "c2.jpg", cv::IMREAD_GRAYSCALE));
 	images.push_back(cv::imread(imagePath + "c3.jpg", cv::IMREAD_GRAYSCALE));
